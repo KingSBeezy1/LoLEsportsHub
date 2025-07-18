39221dcb4d48780879ec4fb6fa2c0e157e344a47
KingSBeezy <delyan.halachev@gmail.com>
2025-07-18 15:24:17 +0300

    UserMatchBookmark class model

---
+++ LoLEsportsHub.Data.Models/UserMatchBookmark.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoLEsportsHub.Data.Models
+{
+    public class UserMatchBookmark
+    {
+        public string ApplicationUserId { get; set; } = null!;
+        public virtual IdentityUser ApplicationUser { get; set; } = null!;
+
+        public Guid MatchId { get; set; }
+        public virtual Match Match { get; set; } = null!;
+
+        public bool IsDeleted { get; set; }
+    }
+}
