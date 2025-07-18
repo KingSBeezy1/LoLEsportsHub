e16153398b79eeabac2833ba1007bb8ea4bb10b5
KingSBeezy <delyan.halachev@gmail.com>
2025-07-18 15:24:44 +0300

    Match class model

---
+++ LoLEsportsHub.Data.Models/Match.cs
@@ -0,0 +1,18 @@
+﻿namespace LoLEsportsHub.Data.Models
+{
+    public class Match
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Region { get; set; } = null!; 
+        public DateTime MatchDate { get; set; }
+        public string Winner { get; set; } = null!;
+        public string? VODUrl { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<UserMatchBookmark> BookmarkedByUsers { get; set; }
+            = new HashSet<UserMatchBookmark>();
+        public virtual ICollection<TournamentMatch> Tournaments { get; set; }
+            = new HashSet<TournamentMatch>();
+    }
+}
