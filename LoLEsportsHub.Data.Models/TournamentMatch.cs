d4ca251dfc2f474b7ade206bc54ba274c74055d4
KingSBeezy <delyan.halachev@gmail.com>
2025-07-18 15:28:06 +0300

    Add TournamentMatch class model

---
+++ LoLEsportsHub.Data.Models/TournamentMatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLEsportsHub.Data.Models
+{
+    public class TournamentMatch
+    {
+        public Guid Id { get; set; }
+
+        public Guid TournamentId { get; set; }
+        public virtual Tournament Tournament { get; set; } = null!;
+
+        public Guid MatchId { get; set; }
+        public virtual Match Match { get; set; } = null!;
+
+        public string ScheduledTime { get; set; } = null!;
+        public int AvailableSlots { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+            = new HashSet<Ticket>();
+    }
+}
