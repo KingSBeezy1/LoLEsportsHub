36b888bdc1c3a3599b56515d228e323ed289677f
KingSBeezy <delyan.halachev@gmail.com>
2025-07-18 15:24:37 +0300

    Ticket class model

---
+++ LoLEsportsHub.Data.Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoLEsportsHub.Data.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Guid TournamentMatchId { get; set; }
+        public virtual TournamentMatch TournamentMatch { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+        public virtual IdentityUser User { get; set; } = null!;
+    }
+}
