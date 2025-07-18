6d1f0cdc041ce381ddfe65bb8360e358472de14f
KingSBeezy <delyan.halachev@gmail.com>
2025-07-18 14:52:37 +0300

    Add project files.

---
+++ LoLEsportsHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoLEsportsHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
