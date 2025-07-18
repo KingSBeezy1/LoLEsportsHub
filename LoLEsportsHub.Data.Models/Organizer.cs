0a02c913af51cbca7652309a2c8ec0d0ca4301b5
KingSBeezy <delyan.halachev@gmail.com>
2025-07-18 15:24:51 +0300

    Organizer class model

---
+++ LoLEsportsHub.Data.Models/Organizer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoLEsportsHub.Data.Models
+{
+    public class Organizer
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public string UserId { get; set; } = null!;
+        public IdentityUser User { get; set; } = null!;
+
+        public ICollection<Tournament> ManagedTournaments { get; set; }
+            = new HashSet<Tournament>();
+    }
+}
